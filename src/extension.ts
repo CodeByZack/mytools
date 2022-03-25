5c0e3eedaf916dec8ad899ce2613f96e5328c85a
dongkai <958059970@qq.com>
2022-03-25 16:23:35 +0800

    修改触发方式

--- src/extension.ts
+++ src/extension.ts
@@ -18,12 +18,12 @@ export function activate(context: vscode.ExtensionContext) {
 
       switch (languageId) {
         case 'javascript':
-          const test = Function('"use strict";return (' + msg + ')')();  
+          const test = Function('"use strict";return (' + msg + ')')();
           const str = JSON.stringify(test);
           msg = str;
           break;
         case 'typescript':
-          const test2 = Function('"use strict";return (' + msg + ')')();  
+          const test2 = Function('"use strict";return (' + msg + ')')();
           const str2 = JSON.stringify(test2);
           msg = str2;
           break;
@@ -31,7 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
         default:
       }
       const interfaceStr = json2ts(msg);
-      if(!interfaceStr){
+      if (!interfaceStr) {
         vscode.window.showInformationMessage('出错了！');
         return;
       }
@@ -40,20 +40,19 @@ export function activate(context: vscode.ExtensionContext) {
       });
     },
   );
-
-  const disposable2 = vscode.languages.registerHoverProvider(
-    ['typescript', 'json', 'javascript'],
-    {
-      provideHover: async (document, position) => {
-        const word = document.getText(
-          document.getWordRangeAtPosition(position),
-        );
-        const translateResult = await translate(word);
-        return new vscode.Hover(`${word}:${translateResult}`);
-      },
+  const disposable2 = vscode.commands.registerCommand(
+    'mytools-zack.translate',
+    async () => {
+      let editor = vscode.window.activeTextEditor!;
+      const document = editor.document;
+      const selection = editor.selection;
+      const selectTxt = document.getText(selection);
+      const translateResult = await translate(selectTxt);
+      editor.edit((v) => {
+        v.replace(selection, translateResult);
+      });
     },
   );
-
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable2);
 }
